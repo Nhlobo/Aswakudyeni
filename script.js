102fba008c764ce681885baf3260e7b1bfe11487
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-12-16 20:16:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Toggle mobile menu
+const menuButton = document.querySelector('.menu-button');
+const menuNav = document.querySelector('header nav');
 
+menuButton.addEventListener('click', () => {
+    menuNav.classList.toggle('open');
+});
+
+// Smooth scroll for menu links
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
